63a4084d07b195f0305994b86baaf22d35bbffb7
Abhishek Sharma <abhisheks@pearlorganisation.com>
2025-05-13 11:02:09 +0530

    1st api

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Default route
+app.get('/', (req, res) => res.send('API is running...'));
+
+// Start server
+const PORT = process.env.PORT || 2025;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
