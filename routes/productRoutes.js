428e1bb0783f2577cdde8f49b6a9848e37a017b7
Abhishek Sharma <abhisheks@pearlorganisation.com>
2025-05-13 17:54:43 +0530

    cool

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../Controllers/productController');
-const { uploadProduct } = require('../middlewares/upload');
+const { uploadProduct } = require('../middleware/upload');//no s
 
 // Updated to handle multipart/form-data and convert to base64
 router.post('/add-product', uploadProduct.array('images'), productController.createProduct);
