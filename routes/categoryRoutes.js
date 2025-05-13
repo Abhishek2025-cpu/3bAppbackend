2d812fc7b0bc02cd06b622ccc54a3d24eca786d9
Abhishek Sharma <abhisheks@pearlorganisation.com>
2025-05-13 15:18:23 +0530

    new

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createCategory } = require('../Controllers/categoryController');
+
+// Correctly importing the full controller object
+const categoryController = require('../Controllers/categoryController');
+
+// Import the multer middleware for category image upload
 const { uploadCat } = require('../middleware/upload');
 
+// POST route to add a new category with image upload
 router.post('/add-category', uploadCat.single('image'), categoryController.createCategory);
 
 module.exports = router;
