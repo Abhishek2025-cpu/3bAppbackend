43b6eee73cab2cb3a834ab6eee93c1d5795fa8b6
Abhishek <abhisheks@pearlorganisation.com>
2025-05-15 16:05:21 +0530

    new

--- utils/fcm.js
+++
@@ -1,9 +0,0 @@
-// utils/fcm.js or config/firebase.js
-const admin = require('firebase-admin');
-const serviceAccount = require('../config/serviceAccountKey.json'); 
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
