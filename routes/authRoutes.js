a3769474781e4404d3d27d84fbad195959866866
Abhishek <abhisheks@pearlorganisation.com>
2025-05-16 13:52:45 +0530

    profileupdateapi

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../Controllers/authController');
-const { uploadPrifleUpload } = require('../middleware/upload');
+const { uploadPrifle } = require('../middleware/upload');
 
-router.post('/signup', uploadPrifleUpload.single('profileImage'), signup);
-router.put('/update-user/:userId', uploadPrifleUpload.single('profileImage'), updateUser);
+router.post('/signup', uploadPrifle.single('profileImage'), signup);
+router.put('/update-user/:userId', uploadPrifle.single('profileImage'), updateUser);
 router.post('/login', login);
 
 module.exports = router;
