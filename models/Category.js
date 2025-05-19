a2a66488aee0ba2a75ba13690b4dddc84562b432
Abhishek <abhisheks@pearlorganisation.com>
2025-05-19 11:09:42 +0530

    multiple image upload in cat

--- models/Category.js
+++ models/Category.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
   categoryId: { type: String, required: true, unique: true },
   name: String,
-  image: {
+  image: [{
     data: Buffer,
     contentType: String,
-  },
+  }],
   position: {
     type: Number,
     default: null, // Optional field
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const categoryController = require('../Controllers/categoryController');
 const { uploadCat } = require('../middleware/upload');
 
-router.post('/add-category', uploadCat.single('image'), categoryController.createCategory);
+router.post('/add-category', uploadCat.array('image'), categoryController.createCategory);
 router.get('/get-categories', categoryController.getCategories);
 
 module.exports = router;
