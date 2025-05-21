9ee4b07335c32d07f52764848f1bd6f342ff0654
Abhishek <abhisheks@pearlorganisation.com>
2025-05-21 12:16:27 +0530

    s

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const categoryController = require('../Controllers/categoryController');
 const { uploadCat } = require('../middleware/upload');
 
-router.post('/add-category', uploadCat.array('image'), categoryController.createCategory);
+router.post('/add-category', uploadCat.array('images'), categoryController.createCategory);
+
 router.get('/get-categories', categoryController.getCategories);
 router.put('/update-categories/:categoryId', uploadCat.array('images'), categoryController.updateCategory);
 router.delete('/delete-categories/:categoryId', categoryController.deleteCategory);
