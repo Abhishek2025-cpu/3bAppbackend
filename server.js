1ef10c72962bd5ba2d1dd9febb66d791268a0bc1
Abhishek <abhisheks@pearlorganisation.com>
2025-05-23 16:22:15 +0530

    new

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ const fs = require('fs');
 const cors = require('cors');
 dotenv.config();
 const app = express();
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
 
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
 
 // Connect to MongoDB
 connectDB();
