a46d0054bd6ac3d4fd5db2428c978bd172ee1fd4
Abhishek <abhisheks@pearlorganisation.com>
2025-06-03 15:08:38 +0530

    test

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -6,8 +6,9 @@ const { uploadCat } = require('../middleware/upload');
 router.post('/add-category', uploadCat.array('images'), categoryController.createCategory);
 
 router.get('/get-categories', categoryController.getCategories);
-router.put('/update-categories/:categoryId', uploadCat.array('images'), categoryController.updateCategory);
-router.delete('/delete-categories/:categoryId', categoryController.deleteCategory);
+router.put('/update-categories/:id', uploadCat.array('images'), categoryController.updateCategory);
+router.delete('/delete-categories/:id', categoryController.deleteCategory);
+
 router.put('/toggle-stock/:id', categoryController.toggleCategoryStock);
 router.get('/category/:id', categoryController.getCategoryById);
 module.exports = router;
